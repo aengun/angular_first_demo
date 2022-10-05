ba82500aab90df667ca07af2ec262e5d2a626b06
aengun <aengun@icloud.com>
2022-10-05 14:40:08 +0900

    component selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]', // >> app.component에서 <div app-servers> 이렇게 사용해야 함
+  // selector: '.app-servers', // >> <div class="app-servers>" 이렇게
+  selector: 'app-servers', // 일반적인 방법
   templateUrl: './servers.component.html',
   // template: `
   //     <app-server></app-server>
